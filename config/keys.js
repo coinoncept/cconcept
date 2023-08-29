2a3f57f9eaf6fe1acdf1c70b70eae3960329b64c
Your Name <you@example.com>
2023-08-29 16:10:21 -0400

    all

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword33 = 'mongodb://localhost:27017/mern2';
+dbPassword = 'mongodb+srv://coinConcept:HelloConcept12@cluster1.ml9qmk1.mongodb.net/CoinConceptUsers'
+module.exports = {
+    mongoURI: dbPassword
+};
